09e7d43934cf882933636df85563f50bcccd2b06
yanivdror <yanivdror25@gmail.com>
2020-12-16 13:53:37 +0200

    second text change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>change</p>
+        <p>change second change</p>
         <a
           className="App-link"
           href="https://reactjs.org"
